ccf04bc588644c08377950f54a07bf32dca972b7
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-10-05 12:41:22 +0400

    добавил контейнер

--- my-fake-app/src/App.jsx
+++ my-fake-app/src/App.jsx
@@ -1,12 +1,13 @@
 import Filter from "./components/Filter";
 import ProductList from "./components/ProductList";
+import Container from "@mui/material/Container";
 
 function App() {
   return (
-    <>
+    <Container maxWidth="m">
       <Filter />
       <ProductList />
-    </>
+    </Container>
   );
 }
 
