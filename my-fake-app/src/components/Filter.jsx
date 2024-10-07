a7da8d385ef8d96c6388dc7bd15ac776281c5040
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-10-07 12:49:44 +0400

    поработал над стилями

--- my-fake-app/src/components/Filter.jsx
+++ my-fake-app/src/components/Filter.jsx
@@ -2,6 +2,11 @@
 import { useDispatch } from "react-redux";
 import { setFilter } from "../store/productsSlice";
 import "./filter.css";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Filter = () => {
   const dispatch = useDispatch();
@@ -12,14 +17,18 @@ const Filter = () => {
 
   return (
     <div>
-      <label htmlFor="filter">Filter by category: </label>
-      <select id="filter" onChange={handleFilterChange}>
-        <option value="">All</option>
-        <option value="men's clothing">Men&#39;s Clothing</option>
-        <option value="women's clothing">Women&#39;s Clothing</option>
-        <option value="jewelery">Jewelery</option>
-        <option value="electronics">Electronics</option>
-      </select>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Category</InputLabel>
+          <Select id="filter" label="Category" onChange={handleFilterChange}>
+            <MenuItem value="">All</MenuItem>
+            <MenuItem value="men's clothing">Men&#39;s Clothing</MenuItem>
+            <MenuItem value="women's clothing">Women&#39;s Clothing</MenuItem>
+            <MenuItem value="jewelery">Jewelery</MenuItem>
+            <MenuItem value="electronics">Electronics</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 };
--- my-fake-app/src/components/ProductList.jsx
+++ my-fake-app/src/components/ProductList.jsx
@@ -2,7 +2,12 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts, selectFilteredProducts } from "../store/productsSlice";
-
+import Box from "@mui/material/Box";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import CurrencyRubleIcon from "@mui/icons-material/CurrencyRuble";
+import "./filter.css";
 const ProductList = () => {
   const dispatch = useDispatch();
   const products = useSelector(selectFilteredProducts);
@@ -19,16 +24,50 @@ const ProductList = () => {
     return <div>Loading...</div>;
   } else if (productsStatus === "succeeded") {
     return (
-      <>
+      <Box
+        className="box__item"
+        style={{ display: "flex", gap: 40, flexWrap: "wrap", marginTop: 50 }}
+      >
         {products.map((product) => (
-          <div key={product.id}>
-            <h2>{product.title}</h2>
-            <p>Price: ${product.price}</p>
-            <p>Category: {product.category}</p>
-            <img src={product.image} alt={product.title} width="100" />
-          </div>
+          <Box style={{ border: "1px solid black" }} key={product.id}>
+            <CardMedia
+              sx={{ width: "100%", height: 150 }}
+              component="img"
+              image={product.image}
+              alt={product.title}
+            />
+            <CardContent>
+              <Typography
+                style={{ width: 300, paddingTop: 20 }}
+                variant="h5"
+                component="div"
+              >
+                {product.title}{" "}
+              </Typography>
+              <Box style={{ display: "flex", alignItems: "center" }}>
+                <Typography
+                  style={{ fontWeight: "bold", fontSize: 20, paddingTop: 9 }}
+                  gutterBottom
+                  variant="p"
+                >
+                  Price: {product.price}
+                </Typography>
+                <CurrencyRubleIcon
+                  sx={{ width: 19 }}
+                  style={{ paddingBottom: 0 }}
+                />
+              </Box>
+              <Typography
+                style={{ fontWeight: "bold", fontSize: 20 }}
+                gutterBottom
+                variant="p"
+              >
+                Category: {product.category}
+              </Typography>
+            </CardContent>
+          </Box>
         ))}
-      </>
+      </Box>
     );
   } else if (productsStatus === "failed") {
     return console.log(error);
